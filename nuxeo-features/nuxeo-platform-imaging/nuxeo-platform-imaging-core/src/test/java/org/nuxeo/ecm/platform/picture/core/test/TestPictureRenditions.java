3e4f92cfc7901e8ad1303d7a3244e9e5dd81526a
Bogdan Stefanescu <stefanescu.bogdan@gmail.com>
2016-09-06 11:40:47 +0200

    NXP-19326 - fixed nuxeo-features tests

--- nuxeo-features/nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/test/java/org/nuxeo/ecm/platform/picture/core/test/TestPictureRenditions.java
+++ nuxeo-features/nuxeo-platform-imaging/nuxeo-platform-imaging-core/src/test/java/org/nuxeo/ecm/platform/picture/core/test/TestPictureRenditions.java
@@ -37,8 +37,8 @@ import org.nuxeo.ecm.platform.rendition.service.RenditionService;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.HotDeployer;
 import org.nuxeo.runtime.test.runner.LocalDeploy;
-import org.nuxeo.runtime.test.runner.RuntimeHarness;
 
 import com.google.inject.Inject;
 
@@ -67,7 +67,7 @@ public class TestPictureRenditions {
     protected RenditionService renditionService;
 
     @Inject
-    protected RuntimeHarness runtimeHarness;
+    protected HotDeployer deployer;
 
     @Test
     public void shouldExposeAllPictureViewsAsRenditions() throws IOException {
@@ -95,8 +95,7 @@ public class TestPictureRenditions {
         List<RenditionDefinition> availableRenditionDefinitions = renditionService.getAvailableRenditionDefinitions(doc);
         assertEquals(7, availableRenditionDefinitions.size());
 
-        runtimeHarness.deployContrib("org.nuxeo.ecm.platform.picture.core",
-                "OSGI-INF/imaging-picture-renditions-override.xml");
+        deployer.deploy("org.nuxeo.ecm.platform.picture.core:OSGI-INF/imaging-picture-renditions-override.xml");
 
         availableRenditionDefinitions = renditionService.getAvailableRenditionDefinitions(doc);
         assertEquals(5, availableRenditionDefinitions.size());
@@ -108,8 +107,5 @@ public class TestPictureRenditions {
         assertEquals(5, availableRenditions.size());
         availableRenditions = renditionService.getAvailableRenditions(doc, true);
         assertEquals(4, availableRenditions.size());
-
-        runtimeHarness.undeployContrib("org.nuxeo.ecm.platform.picture.core",
-                "OSGI-INF/imaging-picture-renditions-override.xml");
     }
 }
